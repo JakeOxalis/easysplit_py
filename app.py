ca316d6e90c1ec3c58e1bfe42aad92fffbe52484
wychen0908 <wychen0908@gmail.com>
2025-09-01 23:30:24 -0500

    add health & hello endpoints

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True, host ="127.0.0.1", port = 5000)
+
+
+@app.get("/api/health")
+def health():
+    return jsonify(status="ok")
+
+@app.get("/api/hello")
+def hello():
+    return jsonify(message="Hello from Flask", ok=True)
